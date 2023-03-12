2f4faf60ff4c6a169c08f36f0dde6e1a3115a9e8
Sabo1901 <hieugia777@gmail.com>
2023-03-12 16:55:39 +0700

    CreateAdmin
    HieuUpdate

---
+++ ChatCoffee/Areas/Admin/Controllers/RoleController.cs
@@ -0,0 +1,40 @@
+﻿using ChatCoffee.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ChatCoffee.Areas.Admin.Controllers
+{
+    public class RoleController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        // GET: Admin/Role
+        public ActionResult Index()
+        {
+            var items = db.Roles.ToList();
+            return View(items);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IdentityRole model)
+        {
+            if(ModelState.IsValid)
+            {
+                var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(db));
+                roleManager.Create(model);
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+    }
+}
