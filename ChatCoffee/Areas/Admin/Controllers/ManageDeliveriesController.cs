1b014b32a0719cf87e3aaee00b70be70dbda8312
PhamTrongQuoc <112259270+PhamTrongQuoc@users.noreply.github.com>
2023-03-20 08:31:03 +0700

    VanChuyen_ThanhToan

---
+++ ChatCoffee/Areas/Admin/Controllers/ManageDeliveriesController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ChatCoffee.Models;
+using ChatCoffee.Models.ModelsDefault;
+
+namespace ChatCoffee.Areas.Admin.Controllers
+{
+    public class ManageDeliveriesController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Admin/ManageDeliveries
+        public ActionResult Index()
+        {
+            return View(db.VANCHUYENs.ToList());
+        }
+
+        // GET: Admin/ManageDeliveries/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            VANCHUYEN vANCHUYEN = db.VANCHUYENs.Find(id);
+            if (vANCHUYEN == null)
+            {
+                return HttpNotFound();
+            }
+            return View(vANCHUYEN);
+        }
+
+        // GET: Admin/ManageDeliveries/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/ManageDeliveries/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MAVT,TENVT,GIA")] VANCHUYEN vANCHUYEN)
+        {
+            if (ModelState.IsValid)
+            {
+                db.VANCHUYENs.Add(vANCHUYEN);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(vANCHUYEN);
+        }
+
+        // GET: Admin/ManageDeliveries/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            VANCHUYEN vANCHUYEN = db.VANCHUYENs.Find(id);
+            if (vANCHUYEN == null)
+            {
+                return HttpNotFound();
+            }
+            return View(vANCHUYEN);
+        }
+
+        // POST: Admin/ManageDeliveries/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MAVT,TENVT,GIA")] VANCHUYEN vANCHUYEN)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(vANCHUYEN).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(vANCHUYEN);
+        }
+
+        // GET: Admin/ManageDeliveries/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            VANCHUYEN vANCHUYEN = db.VANCHUYENs.Find(id);
+            if (vANCHUYEN == null)
+            {
+                return HttpNotFound();
+            }
+            return View(vANCHUYEN);
+        }
+
+        // POST: Admin/ManageDeliveries/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            VANCHUYEN vANCHUYEN = db.VANCHUYENs.Find(id);
+            db.VANCHUYENs.Remove(vANCHUYEN);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
